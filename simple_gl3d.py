77e49b27f0e18994b73d3806c60c505d570fb5ca
circle-oo <woni8708@naver.com>
2022-11-29 21:17:53 +0900

    Implemented camara rotation

--- simple_gl3d.py
+++ simple_gl3d.py
@@ -69,42 +69,26 @@ def getCameraVectors(px, py, pz, ax, ay, az, ux, uy, uz):
     y = np.cross(z, x)
     return (x, y, z)
 
-def getTrackballRotation(sx, sy, dx, dy, w, h):
-    if sx == dx and sy == dy:
-        return np.eye(4)
-    sx = sx - w//2
-    dx = dx - w//2
-    sy = sy - h//2
-    sy = -sy
-    dy = dy - h//2
-    dy = -dy
-    r = min(w, h)/4
-    p = sx**2 + sy**2
-    if p > r**2 / 2:
-        sz = (r**2 / 2) / np.sqrt(p)
-    else:
-        sz = np.sqrt(r**2 - sx**2 - sy**2)
-    p = dx**2 + dy**2
-    if p > r**2 / 2:
-        dz = (r**2 / 2) / np.sqrt(p)
-    else:
-        dz = np.sqrt(r**2 - dx**2 - dy**2)
-    s = np.array([sx, sy, sz])
-    d = np.array([dx, dy, dz])
-    k = np.cross(s, d)
-    sint = np.linalg.norm(k) / (np.linalg.norm(s) * np.linalg.norm(d))
-    cost = np.sqrt(1 - sint ** 2)
-    k /= np.linalg.norm(k)
-    kx, ky, kz = k
-    R = np.array([
-        [0, -kz, ky, 0],
-        [kz, 0, -kx, 0],
-        [-ky, kx, 0, 0],
-        [0, 0, 0, 1]
-    ])
-    R = np.eye(4) + sint * R + (1 - cost) *  R@R
-    R[3][3] = 1
-    return R
+def rotationy(deg):
+    deg *= np.pi / 180
+    c = np.cos(deg)
+    s = np.sin(deg)
+    return np.array([[c, 0, -s, 0],
+                    [0, 1, 0, 0],
+                    [s, 0, c, 0],
+                    [0, 0, 0, 1]], dtype=np.float32)
+
+def rotationx(deg):
+    deg *= np.pi / 180
+    c = np.cos(deg)
+    s = np.sin(deg)
+    return np.array([[1, 0, 0, 0],
+                    [0, c, -s, 0],
+                    [0, s, c, 0],
+                    [0, 0, 0, 1]], dtype=np.float32)
+
+def f(a, b):
+    return (a[0] + b[0], a[1] + b[1], a[2] + b[2])
 
 def drawCube(size=[0.1, 0.1, 0.1], pos=(0, 0, 0)):
     x, y, z = size
@@ -112,8 +96,6 @@ def drawCube(size=[0.1, 0.1, 0.1], pos=(0, 0, 0)):
         (x/2, y/2, -z/2), (x/2, -y/2, -z/2), (-x/2, -y/2, -z/2), (-x/2, y/2, -z/2), 
         (x/2, y/2, z/2), (x/2, -y/2, z/2), (-x/2, -y/2, z/2), (-x/2, y/2, z/2),
     )
-    vertices = np.array(vertices)
-    pos = np.array(pos)
     surfaces = (
         (BOTTOM_LEFT_FRONT, BOTTOM_LEFT_BACK, BOTTOM_RIGHT_BACK, BOTTOM_RIGHT_FRONT),
         (BOTTOM_LEFT_FRONT, BOTTOM_LEFT_BACK, TOP_LEFT_BACK, TOP_LEFT_FRONT),
@@ -135,7 +117,7 @@ def drawCube(size=[0.1, 0.1, 0.1], pos=(0, 0, 0)):
     for surface in surfaces:
         glColor3f(*colors[c])
         for i in surface:
-            glVertex3f(*(vertices[i] + pos))	
+            glVertex3f(*f(vertices[i], pos))	
         c += 1				
     glEnd()
 
@@ -152,7 +134,9 @@ class Viewer:
         self.ry = 0
         self.fov = 0
         self.zoom = 1
-        self.trans = np.array([.0, .0, .0, .0])
+        self.degx = 0.0
+        self.degy = 0.0
+        self.trans = np.array([.0, .0, 2, .0])
         self.w = 800
         self.h = 800
         self.maze = maze.getMaze(MAP_SIZE)
@@ -203,28 +187,10 @@ class Viewer:
         d = 1
         if self.fov > 0:
             d = 1/np.tan(np.radians(self.fov / 2))
-        
-        glPushMatrix()
-        glMatrixMode(GL_PROJECTION)
-        glLoadIdentity()
-        glOrtho(-self.w/self.h, self.w/self.h, -1, 1, 0.0001, 10000)
-        glMatrixMode(GL_MODELVIEW)
-        glLoadIdentity()
-        ax = np.array([-0.75, -0.75, -0.2, 0]) @ self.cameraMatrix
-        pos = np.array([0, 0, 1, 0]) @ self.cameraMatrix
-        at = np.array([0, 0, 0, 0])
-        up = (np.array([0, 1, 0, 0])) @ self.cameraMatrix
-        # gluLookAt(*pos[:3], *at[:3], *up[:3])
-        glMultMatrixf(self.cameraMatrix.transpose())
-        glTranslatef(*ax[:3])
-        self.draw_axis()
-        glPopMatrix()
-
 
         glMatrixMode(GL_PROJECTION)
         glLoadIdentity()
-        # projection matrix
-        # use glOrtho and glFrustum (or gluPerspective) here
+
         glScalef(self.zoom, self.zoom, 1)
         if self.fov == 0:
             glOrtho(-self.w/self.h, self.w/self.h, -1, 1, 0.0001, 10000)
@@ -232,51 +198,28 @@ class Viewer:
             gluPerspective(self.fov, self.w/self.h, 0.0001, 10000)
         glMatrixMode(GL_MODELVIEW)
         glLoadIdentity()
-        # self.draw_axis()
-        # do some transformations using camera view
-        pos = np.array([0, 0, d, 0]) @ self.cameraMatrix + self.trans 
-        at = np.array([0, 0, 0, 0]) + self.trans
+
+        pos = np.array([0, 0, 0, 0]) @ self.cameraMatrix + self.trans 
+        at = np.array([0, 0, -d, 0]) @ self.cameraMatrix + self.trans
         up = (np.array([0, 1, 0, 0])) @ self.cameraMatrix
         M = getCameraView(*(pos[:3]),
             *(at[:3]),
             *(up[:3]),
             self.trans)
         glMultMatrixf(M)
+
         for i in range(MAP_SIZE):
             for j in range(MAP_SIZE):
                 if self.maze[i][j] == 1:
                     drawCube(size=(0.1, 0.5, 0.1), pos=(0.1*i, 0.25, 0.1*j))
                 else:
                     drawCube(size=(0.1, 0.2, 0.1), pos=(0.1*i, 0.1, 0.1*j))
-        # glColor3f(1, 1, 1)
-        # glutSolidTeapot(0.5)
-
+                    
         glutSwapBuffers()
 
     def keyboard(self, key, x, y):
-        if key == b'=':
-            if self.zoom < 1:
-                self.zoom += 0.1
-            else:
-                self.zoom += 1
-            self.zoom = min(self.zoom, 5)
-            self.zoom = max(self.zoom, 0.1)
-        if key == b'-':
-            if self.zoom < 1:
-                self.zoom -= 0.1
-            else:
-                self.zoom -= 1
-            self.zoom = min(self.zoom, 5)
-            self.zoom = max(self.zoom, 0.1)
-        if key == b'd':
-            self.trans = np.array([.0, .0, .0, .0])
-            self.cameraMatrix = np.eye(4)
-            self.zoom = 1
-        if key == b'0':
-            self.fov = 0
         if key == b'\x1b':
             exit()
-        # glutPostRedisplay()
 
     def special(self, key, x, y):
         if key == 101:
@@ -285,7 +228,6 @@ class Viewer:
         if key == 103:
             self.fov -= 5
             self.fov = max(self.fov, 0)
-        # glutPostRedisplay()
 
     def mouse(self, button, state, x, y):
         if button == GLUT_LEFT_BUTTON:
@@ -295,19 +237,22 @@ class Viewer:
                 self.ry = y
             if state == 1:
                 self.mode = 0
-        if button == GLUT_RIGHT_BUTTON:
-            if state == 0:
-                self.mode = 2
-                self.rx = x
-                self.ry = y
-            if state == 1:
-                self.mode = 0
-
-        # glutPostRedisplay()
 
     def motion(self, x, y):
         if self.mode == 1:
-            self.cameraMatrix = getTrackballRotation(self.rx, self.ry, x, y, self.w, self.h) @ self.cameraMatrix
+            d = 1
+            if x > self.rx:
+                self.degy -= d
+            if x < self.rx:
+                self.degy += d
+            if y > self.ry:
+                self.degx += d
+            if y < self.ry:
+                self.degx -= d
+            self.degx = max(self.degx, -90)
+            self.degx = min(self.degx, 90)
+            print(self.degx, self.degy)
+            self.cameraMatrix = rotationx(self.degx) @ rotationy(self.degy)
             self.rx = x
             self.ry = y
         if self.mode == 2:
@@ -319,16 +264,13 @@ class Viewer:
             self.trans += (y - self.ry) * np.append(vy, [0]) * 0.002
             self.rx = x
             self.ry = y
-        # glutPostRedisplay()
 
     def reshape(self, w, h):
         print(f"window size: {w} x {h}")
         t = min(w, h)
-        # glViewport((w-t)//2, (h-t)//2, t, t)
         glViewport(0, 0, w, h)
         self.w = w
         self.h = h
-        # glutPostRedisplay()
 
     def timer(self, value):
         glutPostRedisplay()
